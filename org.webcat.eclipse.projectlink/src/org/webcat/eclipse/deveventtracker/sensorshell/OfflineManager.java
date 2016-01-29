8746a6d8693d76465c8c18a658021ed0a3e40582
Ayaan Kazerouni <ayaan@vt.edu>
2016-01-29 14:30:25 -0500

    Writes all data to one file 'offlineData.data' instead of to different files separated by timestamp. Gets renamed to postData.data when the time comes to send it to the server.

--- org.webcat.eclipse.projectlink/src/org/webcat/eclipse/deveventtracker/sensorshell/OfflineManager.java
+++ org.webcat.eclipse.projectlink/src/org/webcat/eclipse/deveventtracker/sensorshell/OfflineManager.java
@@ -74,10 +74,7 @@ public class OfflineManager {
 	public void store(SensorDatas sensorDatas) {
 		if (sensorDatas.getSensorData().size() > 0) {
 			synchronized (this) {
-				SimpleDateFormat fileTimestampFormat = new SimpleDateFormat(
-						"yyyy.MM.dd.HH.mm.ss.SSS", Locale.US);
-				String fileStampString = fileTimestampFormat.format(new Date());
-				File outFile = new File(this.offlineDir, fileStampString + ".data");
+				File outFile = new File(this.offlineDir, "offlineData.data");
 				try {		
 					FileWriter fw = new FileWriter(outFile);
 					BufferedWriter out = new BufferedWriter(fw);
@@ -127,28 +124,25 @@ public class OfflineManager {
 	public void recover() throws SensorShellException {
 		System.out.println("Recovering...");
 		// Return immediately if there are no offline files to process.
-		File[] filesToPost;
+		File offlineData = new File(this.offlineDir, "offlineData.data");
+		File postData = new File(this.offlineDir, "postData.data");
+		
+		if (!offlineData.exists()) {
+			return;
+		}
+		
 		synchronized (this) {
 			try {
-				filesToPost = this.offlineDir.listFiles(new ExtensionFileFilter(
-						".data"));
-				for (File current : filesToPost) {
-					File renamed = new File(this.offlineDir, current.getName() + "post");
-					current.renameTo(renamed);
-				}
+				offlineData.renameTo(postData);
 			} catch (Exception e) {
 				Activator.getDefault().log(e);
 			}
 		}
-		File[] xmlFiles = this.offlineDir.listFiles(new ExtensionFileFilter(
-				".datapost"));
-		if (xmlFiles.length == 0) {
-			return;
-		}
+
 		// Tell the parent shell log that we're going to try to do offline
 		// recovery.
-		parentShell.println("Invoking offline recovery on " + xmlFiles.length
-				+ " files.");
+//		parentShell.println("Invoking offline recovery on " + xmlFiles.length
+//				+ " files.");
 
 		// Create a new properties instance with offline recovery/storage
 		// disabled.
@@ -172,54 +166,53 @@ public class OfflineManager {
 		// Create the offline sensor shell to be used for sending this data.
 		SingleSensorShell shell = new SingleSensorShell(shellProps, false,
 				offlineTool);
-		shell.println("Invoking offline recovery on " + xmlFiles.length
-				+ " files.");
+//		shell.println("Invoking offline recovery on " + xmlFiles.length
+//				+ " files.");
 		FileInputStream fileStream = null;
 
 		// For each offline file to recover
-		for (int i = 0; i < xmlFiles.length; i++) {
-			try {
-				// Reconstruct the SensorDatas instances from the serialized
-				// files.
-				shell.println("Recovering offline data from: "
-						+ xmlFiles[i].getName());
-				fileStream = new FileInputStream(xmlFiles[i]);
-				SensorDatas sensorDatas = makeSensorDatasFromFile(xmlFiles[i]);
-				shell.println("Found " + sensorDatas.getSensorData().size()
-						+ " instances.");
+		try {
+			// Reconstruct the SensorDatas instances from the serialized
+			// files.
+			shell.println("Recovering offline data from: "
+					+ postData.getName());
+			fileStream = new FileInputStream(postData);
+			SensorDatas sensorDatas = makeSensorDatasFromFile(postData);
+			shell.println("Found " + sensorDatas.getSensorData().size()
+					+ " instances.");
 //				for (SensorData data : sensorDatas.getSensorData()) {
 //					shell.add(data);
 //				}
 //				// Try to send the data.
 //				shell.println("About to send data");
 //				int numSent = shell.send();
-				SensorBaseClient.getInstance().putSensorDataBatch(sensorDatas);
-				int numSent = sensorDatas.getSensorData().size();
-				shell.println("Successfully sent: " + numSent + " instances.");
-				// If all the data was successfully sent, then we delete the
-				// file.
-				fileStream.close();
+			SensorBaseClient.getInstance().putSensorDataBatch(sensorDatas);
+			int numSent = sensorDatas.getSensorData().size();
+			shell.println("Successfully sent: " + numSent + " instances.");
+			// If all the data was successfully sent, then we delete the
+			// file.
+			fileStream.close();
 //				if (numSent == sensorDatas.getSensorData().size()) {
-					boolean isDeleted = xmlFiles[i].delete();
-					System.out.println(xmlFiles[i].getName() + " was deleted: " + isDeleted);
-					shell.println("Trying to delete " + xmlFiles[i].getName()
-							+ ". Success: " + isDeleted);
+				boolean isDeleted = postData.delete();
+				System.out.println(postData.getName() + " was deleted: " + isDeleted);
+				shell.println("Trying to delete " + postData.getName()
+						+ ". Success: " + isDeleted);
 //				} else {
 //					shell.println("Did not send all instances. " + xmlFiles[i]
 //							+ " not deleted.");
 //				}
-			} catch (Exception e) {
-				shell.println("Error recovering data from: " + xmlFiles[i]);
+		} catch (Exception e) {
+			shell.println("Error recovering data from: " + postData);
+			Activator.getDefault().log(e);
+			try {
+				fileStream.close();
+			} catch (Exception f) {
+				shell.println("Failed to close: " + fileStream.toString()
+						+ " " + e);
 				Activator.getDefault().log(e);
-				try {
-					fileStream.close();
-				} catch (Exception f) {
-					shell.println("Failed to close: " + fileStream.toString()
-							+ " " + e);
-					Activator.getDefault().log(e);
-				}
 			}
 		}
+		
 		
 		shell.quit();
 	}
